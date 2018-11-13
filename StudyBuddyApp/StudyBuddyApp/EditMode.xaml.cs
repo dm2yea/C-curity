7025cca1929c0058d6d659f132e8ca9865dac3b1
Joshua A Duke <jduke14@lsu.edu>
2018-11-13 03:24:13 -0600

    Added an xml file creator when entering edit mode

--- StudyBuddyApp/StudyBuddyApp/EditMode.xaml.cs
+++ StudyBuddyApp/StudyBuddyApp/EditMode.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Xml;
 
 namespace StudyBuddyApp
 {
@@ -23,6 +24,33 @@ namespace StudyBuddyApp
         public EditMode()
         {
             InitializeComponent();
+            // As soon as you enter edit mode, creates the XmlDocument.
+            // the path is StudyBuddyApp/bin/Debug, it's in debug rn because that's how we're running it, 
+            // when we actually make the exe it will be in the release folder I believe
+            // to get to that folder you can right click on the solution explorer and open folder in file explorer
+            XmlDocument doc = new XmlDocument();
+            doc.LoadXml("<item><name>wrench</name></item>");
+
+            // Add a price element.
+            XmlElement newElem = doc.CreateElement("price");
+            newElem.InnerText = "10.95";
+            doc.DocumentElement.AppendChild(newElem);
+
+            XmlWriterSettings settings = new XmlWriterSettings();
+            settings.Indent = true;
+            // Save the document to a file and auto-indent the output.
+            XmlWriter writer = XmlWriter.Create("data.xml", settings);
+            doc.Save(writer);
+
+            //this is just me messing with label names and seeing if we can change them after they are created
+            string test = Chapter1Label.ToString();
+            Console.WriteLine(test);
+            Console.WriteLine(Chapter1Label.Name);
+            Chapter1Label.Name = "Label1";
+            Chapter1Label.Content = "TESTING";
+            Console.WriteLine(Chapter1Label.Name);
+            Label label2 = new Label();
+            label2.Content = "SOMETHING";
         }
     }
 }
